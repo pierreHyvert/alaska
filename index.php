1b0a44d60338bbc052f951f4462a912738bd86eb
phyvert <contact@phyvert.com>
2019-05-20 19:56:00 +0200

    Derniers ajustements

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php session_start();
+
+
 require('controller/frontend.php');
 require('controller/backend.php');
 
--- model/Manager.php
+++ model/Manager.php
@@ -1,9 +1,19 @@
 <?php
 namespace Alaska\Model;
-
-class Manager{
+$local = true;
+if ($local){
+  class Manager{
     protected function dbConnect() {
-        $db = new \PDO('mysql:host=phyvertbdd.mysql.db;dbname=phyvertbdd;charset=utf8', 'phyvertbdd', 'chiwawA12');
+        $db = new \PDO('mysql:host=localhost;dbname=alaska;charset=utf8', 'root', '');
         return $db;
     }
+  }
+}
+  else {
+    class Manager{
+        protected function dbConnect() {
+          $db = new \PDO('mysql:host=phyvertbdd.mysql.db;dbname=phyvertbdd;charset=utf8', 'phyvertbdd', 'chiwawA12');
+          return $db;
+        }
+      }
 }
