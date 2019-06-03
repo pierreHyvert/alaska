2967d998ce745dffb0fc0cf470224eefb9994a86
pierreHyvert <42735751+pierreHyvert@users.noreply.github.com>
2019-06-03 12:23:33 +0200

    Update Manager.php

--- model/Manager.php
+++ model/Manager.php
@@ -12,7 +12,7 @@ if ($local){
   else {
     class Manager{
         protected function dbConnect() {
-          $db = new \PDO('mysql:host=phyvertbdd.mysql.db;dbname=phyvertbdd;charset=utf8', 'phyvertbdd', 'chiwawA12');
+          $db = new \PDO('mysql:host=phyvertbdd.mysql.db;dbname=phyvertbdd;charset=utf8', '', '');
           return $db;
         }
       }
